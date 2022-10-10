8d0522bacf96752ac3f0fa9b8a9ead2fd45061df
Raquel <raquel.abarca@datagrove.com>
2022-10-10 13:52:06 -0600

    different pages added

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vitepress'
+
+export default ({
+    description: 'Software That Does Good',
+    title: 'Datagrove Costa Rica',
+})
--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,8 @@
 # Hello World
 ## This is a test
+
+I am trying to do something cool
+
+### All these options work!
+
+[Getting started](/getting-started.md)
