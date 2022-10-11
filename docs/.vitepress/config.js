bd6db06f04cb6a2a60fe6a11cdf81659f713d4e1
Raquel <raquel.abarca@datagrove.com>
2022-10-11 14:13:28 -0600

    name added

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,4 +3,5 @@ import { defineConfig } from 'vitepress'
 export default ({
     description: 'Software That Does Good',
     title: 'Datagrove Blog',
+    logo: '/bright_green_circle.png',
 })
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,5 @@
 # Hello World
+
 ## This is a test
 
 I am trying to do something cool
